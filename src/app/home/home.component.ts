dbed54ccb2b36a54971ed29558c0b513b6207cab
ThomasGately <20bottless@gmail.com>
2021-02-12 11:37:50 +0000

    formated woekspace

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { AccountService } from '@app/_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
-    account = this.accountService.accountValue;
+  account = this.accountService.accountValue;
 
-    constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
 }
